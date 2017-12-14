fa45887f6bbdc6f81444d70b47f60ad39145734f
ahmad blehed <ahmad.blehed@gmail.com>
2017-12-14 18:52:29 +0300

    i am an idiot

--- src/cs210_project/ArrayList.java
+++ src/cs210_project/ArrayList.java
@@ -1,10 +1,50 @@
 package cs210_project;
 public class ArrayList {
-    private Tweeto head;
-    private Tweeto Tail;
+    private Tweeto Head;
     private int Size=0;
 
+    public Tweeto getHead() {
+        return Head;
+    }
 
+    public void setHead(Tweeto head) {
+        this.Head = head;
+    }
+
+    public int getSize() {
+        return Size;
+    }
+
+    public void setSize(int Size) {
+        this.Size = Size;
+    }
+
+    public void addfirst(Tweeto N){
+        if (this.Head==null){
+          Head=N;
+          
+        }
+          else{
+         N.getNext().setNext(Head);
+              Head=N;
+              
+                  }
+        Size++;
+     }
+     public Tweeto search(String ID){
+        if (Head== null)
+            return null;
+        Tweeto temp= Head;
+        
+        while(temp!=null){
+            if (temp.getID()== ID){
+                return temp;
+            }
+            temp=temp.getNext();
+                }
+        return temp;
+        
+        
+    }
     
-
 }
