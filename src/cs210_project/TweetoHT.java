6ebd6b6e0ec8ca7a1914963c84d9cb797ab4d95a
faris saleem <faris.al.saleem15@gamil.com>
2017-12-16 19:36:17 +0300

    implemented hashtable

--- src/cs210_project/TweetoHT.java
+++ src/cs210_project/TweetoHT.java
@@ -1,5 +1,49 @@
 package cs210_project;
 
 public class TweetoHT {
+    private ArrayList [] HT;
+    private final int size = 36; 
+
+    public TweetoHT() {
+        HT = new ArrayList[getSize()];
+            
+    }
+    
+    public void add (Tweeto t){
+    int i = (t.hashCode() % 36);
+    if (HT[i] == null)
+        HT[i] = new ArrayList();
+    HT[i].addfirst(t);
+    }
+    
+    public int search(String u, String k){
+    int c =0;
+    int ucode=(int) u.charAt(0);
+    int i = (ucode % 36);
+    if (HT[i]==null)
+        return 0;
+    else
+        return HT[i].search(u, k);
+    } 
+    
+
+    public void setHT(ArrayList[] HT) {
+        this.HT = HT;
+    }
+    
+
+    public int getSize() {
+        return size;
+    }
+    public ArrayList[] getHT() {
+        return HT;
+    }
+    
+    
+    
+    
+    
+    
+    
     
 }
