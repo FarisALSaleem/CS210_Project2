f1da0160bd45ddc285242161db5a04cbf3710d61
Faris ALSaleem <33284935+FarisALSaleem@users.noreply.github.com>
2017-12-18 00:36:33 +0300

    added heap

---
+++ src/cs210_project/Arraylist.java
@@ -0,0 +1,56 @@
+package cs210_project;
+
+public class Arraylist {
+
+    private Tweeto Head;
+    private int Size = 0;
+
+    public Arraylist() {
+    }
+
+    public Tweeto getHead() {
+        return Head;
+    }
+
+    public void setHead(Tweeto head) {
+        this.Head = head;
+    }
+
+    public int getSize() {
+        return Size;
+    }
+
+    public void setSize(int Size) {
+        this.Size = Size;
+    }
+
+    public void addfirst(Tweeto N) {
+        if (getSize()== 0) {
+            Head = N;
+        } else {
+            N.setNext(Head);
+            Head = N;
+        }
+        Size++;
+    }
+
+    public int search(String u, String k) {
+        int c = 0;
+        if (Head == null) {
+            return c;
+        }
+        Tweeto temp = Head;
+
+        while (temp != null) {
+            if (temp.getID().charAt(1)== u.charAt(0) || temp.getID().charAt(1) == u.charAt(0)-32){
+                if (temp.getTweet().contains(k)) {
+                    c++;
+                }
+            }
+            temp = temp.getNext();
+        }
+        return c;
+
+    }
+
+}
---
+++ src/cs210_project/Heap.java
@@ -0,0 +1,40 @@
+package cs210_project;
+
+public class Heap {
+
+    private Tweeto root;
+    private int size = 0;
+
+    public Heap(Tweeto root) {
+        setRoot(root);
+        size++;
+    }
+
+    public Tweeto getRoot() {
+        return root;
+    }
+
+    public Heap() {
+    }
+
+    public void setRoot(Tweeto root) {
+        this.root = root;
+    }
+
+    public void add(Tweeto t) {
+        if (getSize() == 0) {
+            setRoot(t);
+            size++;
+        }
+        
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+}
