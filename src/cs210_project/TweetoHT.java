b1bcf85673e6991ace706a55bf0c4de1a7c11093
Faris ALSaleem <33284935+FarisALSaleem@users.noreply.github.com>
2017-12-18 00:45:32 +0300

    deleted c

--- src/cs210_project/TweetoHT.java
+++ src/cs210_project/TweetoHT.java
@@ -1,23 +1,22 @@
 package cs210_project;
 
 public class TweetoHT {
-    private ArrayList [] HT;
+    private Arraylist [] HT;
     private final int size = 36; 
 
     public TweetoHT() {
-        HT = new ArrayList[getSize()];
+        HT = new Arraylist[getSize()];
             
     }
     
     public void add (Tweeto t){
     int i = (t.hashCode() % 36);
     if (HT[i] == null)
-        HT[i] = new ArrayList();
+        HT[i] = new Arraylist();
     HT[i].addfirst(t);
     }
     
     public int search(String u, String k){
-    int c =0;
     int ucode=(int) u.charAt(0);
     int i = (ucode % 36);
     if (HT[i]==null)
@@ -27,7 +26,7 @@ public class TweetoHT {
     } 
     
 
-    public void setHT(ArrayList[] HT) {
+    public void setHT(Arraylist[] HT) {
         this.HT = HT;
     }
     
@@ -35,15 +34,7 @@ public class TweetoHT {
     public int getSize() {
         return size;
     }
-    public ArrayList[] getHT() {
+    public Arraylist[] getHT() {
         return HT;
     }
-    
-    
-    
-    
-    
-    
-    
-    
 }
