057b76dafdadcf0af740bc3a271efd88f2474d0a
Faris ALSaleem <33284935+FarisALSaleem@users.noreply.github.com>
2017-12-18 00:42:49 +0300

    fixed some stuff

--- src/cs210_project/Test.java
+++ src/cs210_project/Test.java
@@ -1,7 +1,10 @@
 package cs210_project;
-
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Test {
@@ -38,37 +41,62 @@ public class Test {
         System.out.printf("Total Time: %f\n", s);
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
 
         Scanner input = new Scanner(System.in);
-
         String textfile = input.next();
         File infile = new File(textfile);
+        int numberOfTweets = 0;
 
         int d = input.nextInt();
         String u = input.next();
         String k = input.next();
-        input = new Scanner(infile);
-
+        double startTime = System.nanoTime();
+        
         if (isIllegalValue(d, u)) {
             System.out.println("stop");
-        } else if (d == 1) {
-            ArrayList list = new ArrayList();
-            String tempString = "";
-            String id = "";
-            String tweet = "";
-            while (input.hasNext()) {
-                String[] tempString2 = input.nextLine().split("Wed Dec 14"); 
-                tempString = tempString2[0].substring(tempString2[0].indexOf(" ")+1);
-                System.out.println(tempString);              
-              }
-            
-            //printOut(list.search(u, k), 0);
+        }
+        
+        String document = "";
+        BufferedReader br = null;
+        br = new BufferedReader(new FileReader(infile));
+        String line = "";
+        while ((line = br.readLine()) != null) {
+            document += line + "\n";
+        }
+        ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> tweet = new ArrayList<>();
+        ArrayList<String> id = new ArrayList<>();
+        while (!document.isEmpty()) {
+            list.add(document.substring(document.indexOf(" ") + 1, document.indexOf("Wed Dec 14")));
+            document = document.substring(document.indexOf("Wed Dec 14") + 30);
+        }
 
+        for (int i = 0; i < list.size(); i++) { 
+            id.add(list.get(i).substring(0,list.get(i).indexOf(" ")));
+            tweet.add(list.get(i).substring(list.get(i).indexOf(" ") + 1));
+            if (tweet.get(i).contains("@"))
+                tweet.set(i , tweet.get(i).substring(tweet.get(i).indexOf(" ", tweet.get(i).indexOf("@"))));
+        }
+
+        if (d == 1) {
+            Arraylist a = new Arraylist();
+            for (int i = 0; i < list.size(); i++) {
+                a.addfirst(new Tweeto(id.get(i), tweet.get(i)));
+            }
+            numberOfTweets = a.search(u, k);
+            double endTime = System.nanoTime();
+            printOut(numberOfTweets, (endTime-startTime)/1e+9);
+            
+            System.out.println(a.search(u,k));
         } else if (d == 2) {
             System.out.println("implemented yet");
+            double endTime = System.nanoTime();
+            printOut(numberOfTweets, (endTime-startTime)/1e+9);
         } else {
             System.out.println("implemented yet");
+            double endTime = System.nanoTime();
+            printOut(numberOfTweets, (endTime-startTime)/1e+9);
         }
 
     }
