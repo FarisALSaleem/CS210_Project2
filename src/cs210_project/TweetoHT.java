aafdc935d4a26111536dfcdaa94690c692f314ad
faris saleem <faris.al.saleem15@gamil.com>
2017-12-21 19:48:08 +0300

    happy

--- src/cs210_project/TweetoHT.java
+++ src/cs210_project/TweetoHT.java
@@ -1,40 +1,53 @@
 package cs210_project;
 
 public class TweetoHT {
-    private Arraylist [] HT;
-    private final int size = 37; 
+
+    private Tweeto[] HT;
+    private final int size = 37;
 
     public TweetoHT() {
-        HT = new Arraylist[getSize()];
-            
-    }
-    
-    public void add (Tweeto t){
-    int i = (t.hashCode() % 37);
-    if (HT[i] == null)
-        HT[i] = new Arraylist();
-    HT[i].addfirst(t);
-    }
-    
-    public int search(String u, String k){
-    int ucode=(int) u.charAt(0);
-    int i = (ucode % 37);
-    if (HT[i]==null)
-        return 0;
-    else
-        return HT[i].search(u, k);
-    } 
-    
+        HT = new Tweeto[getSize()];
 
-    public void setHT(Arraylist[] HT) {
+    }
+
+    public void add(Tweeto t) {
+        int i = (t.hashCode() % 37);
+        if (HT[i] == null) {
+            HT[i] = new Tweeto(t.getID(), t.getTweet());
+        } else {
+            t.setNext(HT[i]);
+            HT[i] = t;
+        }
+    }
+
+    public int search(String u, String k) {
+        int ucode = (int) u.charAt(0);
+        int i = (ucode % 37);
+        Tweeto temp = HT[i];
+        int c = 0;
+        if (HT[i] == null) 
+            return 0;
+        
+        else {
+            while (temp != null) {
+                if (temp.getID().toLowerCase().charAt(1) == u.toLowerCase().charAt(0)) {
+                    if (temp.getTweet().contains(k)) 
+                        c++;
+                }temp = temp.getNext();
+            }
+            return c;
+        }
+    }
+
+    public void setHT(Tweeto[] HT) {
         this.HT = HT;
     }
-    
 
     public int getSize() {
         return size;
     }
-    public Arraylist[] getHT() {
+
+    public Tweeto[] getHT() {
         return HT;
     }
 }
