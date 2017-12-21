5a95d475ba0a85ea272e32b8241a7d6384cfeee4
ahmad blehed <ahmad.blehed@gmail.com>
2017-12-22 01:06:31 +0300

    I am garbage

--- src/cs210_project/Heap.java
+++ src/cs210_project/Heap.java
@@ -2,39 +2,93 @@ package cs210_project;
 
 public class Heap {
 
-    private Tweeto root;
-    private int size = 0;
-
-    public Heap(Tweeto root) {
-        setRoot(root);
-        size++;
-    }
-
-    public Tweeto getRoot() {
-        return root;
-    }
-
+    private Tweeto[] heap;
+    private int size;
+    
     public Heap() {
+        heap= new Tweeto[1000001];
+        size=0;
     }
 
-    public void setRoot(Tweeto root) {
-        this.root = root;
-    }
 
-    public void add(Tweeto t) {
-        if (getSize() == 0) {
-            setRoot(t);
-            size++;
-        }
-        
+   public void insert(Tweeto T){
+        //inserts Tweeto T in appropriate position.
+        size++;
+        heap[size] = T;
+        swim(size);   
     }
+    
     
     public int getSize() {
         return size;
     }
-
-    public void setSize(int size) {
-        this.size = size;
+    private void swap(int k, int i) {
+        // swap elements
+        Tweeto tmp = heap[k];
+        heap[k] = heap[i];
+        heap[i] = tmp;
+        
     }
+
+    
+     private void swim(int k){
+        
+        // if k has parent and is bigger than child, swap
+        while (k > 1 && 
+                (heap[k].compareTo(heap[k/2]) > 0)) {
+            swap(k, k/2);
+            // K becomes the parent
+            k = k/2;
+        }
+    }
+    private void sink(int k){
+        
+        while (2 * k <= size){
+            //start with left child
+            int biggerChild = k * 2;
+            
+             //compare left child with right child 
+            if ( 2 * k + 1 <= size && 
+                   heap[k*2+1].compareTo(heap[2*k]) > 0){
+               biggerChild = 2 * k + 1;
+            }
+             //compare parent with smaller child
+            if (heap[biggerChild].compareTo(heap[k]) > 0){
+                swap(k, biggerChild);
+               
+            }else{
+                //we're done, get out
+                break;
+            }
+            k = biggerChild;     
+        }
+    }
+    private Tweeto getTweeto (int x){
+        return heap[x];
+    }
+    
+    public Tweeto remove(){
+        if (isEmpty()){
+            return null;
+        }
+        //get tweeto
+        Tweeto result = heap[1];
+        
+        //remove last child
+        heap[1] = heap[size];
+        heap[size] = null;
+        size--;
+        
+        sink(1);
+        
+        return result;
+    }
+     public boolean isEmpty(){ 
+        return size == 0;
+    } 
+    
+    
+    
+    
 
 }
