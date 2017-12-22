be2c0dd74cfe7e9ce5f216805cb330b7c36822c4
faris saleem <faris.al.saleem15@gamil.com>
2017-12-22 16:36:34 +0300

    fix

--- src/cs210_project/Tweeto.java
+++ src/cs210_project/Tweeto.java
@@ -48,10 +48,6 @@ public class Tweeto implements Comparable<Tweeto>{
     public int hashCode() {
         if ((getID().toLowerCase().charAt(1) == 'z'))
             return 96;
-        else if ((getID().toLowerCase().charAt(1) == 'x'))
-            return 94;
-        else if ((getID().toLowerCase().charAt(1) == 'y'))
-            return 95;
         return ((int)(getID().toLowerCase().charAt(1)));
     }
 
